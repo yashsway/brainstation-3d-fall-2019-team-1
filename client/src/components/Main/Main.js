594787c4ffb9e556a056dd1a2cbb168edb9a4623
Heather Alsen <halsen@kepler.space>
2019-11-22 14:51:54 -0500

    stuff

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import "./Main.scss";
 import girlImage from '../../assets/Image1.svg';
+import phone from '../../assets/phone.png';
+import desktop from '../../assets/desktop.png';
+import people from '../../assets/people.png';
 
 export class Main extends Component {
     render() {
@@ -10,8 +13,8 @@ export class Main extends Component {
             <section className="contentsection one">
                 <span className="chart-title"> The Important Infomation</span>
                 <div className="button-container">
-                <button className="chart-button">Chart 1</button>
-                <button className="chart-button">Chart 2</button>
+                <button className="chart-button">Benefits</button>
+                <button className="chart-button">Barriers</button>
                 <button className="chart-button">Chart 3</button>
                 </div>
 
@@ -21,9 +24,39 @@ export class Main extends Component {
             </section>
 
             <section className="contentsection two">
-                <h1>test</h1>
+                <div className="image-container">
                 <img src={girlImage} className="girlImage" ></img>
-            </section>
+                </div>
+
+                <div className="sectiontwo-container">
+                 <h1>test</h1>  
+                 <p className="paragraph secondsection" >fnjdlas nbfjkldsabnjlfbdsad fasbdsna jklbnfakl wbfh kdl sa bhkfdsbak fnjdlas nbfjkldsabnjlfbdsad fasbdsna jklbnfakl wbfhkdlsab hkfdsbak fnjdlas nbfjkldsabnjlfbdsad fasbdsna jklbnfakl wbfhkdlsabhkfd sbak</p> 
+                </div>
+                
+                
+                
+            </section> 
+
+            <section className="contentsection three">
+                {/* <div className="percentage-container"> */}
+
+                    <div className="percentage-content-container">
+                    <img className="phone-img" src={phone} alt="hi"></img>
+                    <span className="percentage">40%</span>
+                    </div>
+
+                    <div className="percentage-content-container">
+                    <img className="desktop-img" src={desktop} alt="hi"></img>
+                    <span className="percentage">40%</span>
+                    </div>
+
+                    <div className="percentage-content-container">
+                    <img className="people-img" src={people} alt="hi"></img>
+                    <span className="percentage">40%</span>
+                    </div>
+
+                {/* </div> */}
+            </section> 
             </>
         )
     }
