0df7bd02e5a2b0c2d5d69abde6d5332aa318110b
Amy Latimer <56357845+amylatimer@users.noreply.github.com>
2019-11-22 12:31:43 -0500

    finished skeleton for switch component, tested a sample bar chart

--- client/src/components/Switch/Switch.js
+++ client/src/components/Switch/Switch.js
@@ -1,10 +1,80 @@
 import React from 'react';
+import './Switch.scss';
+import SampleBarChart from '../SampleBarChart/SampleBarChart';
 
 class Switch extends React.Component {
-    state = {}
+    state = {
+        displayOne: true,
+        displayTwo: false,
+        displayThree: false,
+    }
+
+    toggleOne() {
+        this.setState({
+            displayOne: true,
+            displayTwo: false,
+            displayThree: false,
+        })
+    }
+
+    toggleTwo() {
+        this.setState({
+            displayOne: false,
+            displayTwo: true,
+            displayThree: false,
+        })
+    }
+
+    toggleThree() {
+        this.setState({
+            displayOne: false,
+            displayTwo: false,
+            displayThree: true,
+        })
+    }
+
     render() {
         return (
-            <h1> This is the chart switch component </h1>
+            <section className="switch-section">
+                <div className="switch-section__container">
+
+
+
+                    <div className="switch-section__button-container">
+                        <button className="switch-section__button" onClick={() => this.toggleOne()}>Chart 1</button>
+                        <button className="switch-section__button" onClick={() => this.toggleTwo()}>Chart 2</button>
+                        <button className="switch-section__button" onClick={() => this.toggleThree()}>Chart 3</button>
+                    </div>
+
+                    {/* <iframe
+                        src="https://public.tableau.com/views/BrainStationKickstarterNumbers/1?:showVizHome=n&amp;:embed=true&amp;:display_count=yes"
+                        width="645" height="955">
+                    </iframe> */}
+
+                    {/* <iframe
+                        src="https://public.tableau.com/views/BrainStationKickstarterNumbers/ofProjectsDetail?:showVizHome=n&amp;:embed=true&amp;:display_count=yes"
+                        width="645" height="300">
+                    </iframe> */}
+
+                    {
+                        (this.state.displayOne === true) ?
+                            <div className="chart-caption">
+                                <iframe
+                                    src="https://public.tableau.com/views/BrainStationKickstarterNumbers/ofProjectsDetail?:showVizHome=n&amp;:embed=true&amp;:display_count=yes"
+                                    width="645" height="300">
+                                </iframe>
+                                <p>This is a caption for the chart</p>
+                            </div> : null
+                    } {
+                        (this.state.displayTwo === true) ? <iframe
+                            src="https://public.tableau.com/views/BrainStationKickstarterNumbers/1?:showVizHome=n&amp;:embed=true&amp;:display_count=yes"
+                            width="645" height="300">
+                        </iframe> : null
+                    } {
+                        (this.state.displayThree === true) ? <SampleBarChart /> : null
+                    }
+                </div>
+            </section>
         );
     }
 }
